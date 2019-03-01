de8fe328eb7c8b6b4a8d5609be2eae6d807f6f50
unknown <nouirainsaf@gmail.com>
2019-03-01 17:26:19 +0100

    commit 2

--- form.js
+++ form.js
@@ -1,7 +1,7 @@
 
+//validate mail
 
-
-        function ValidateEmail()
+        function validateEmail()
         {
                 let email = document.getElementById('email');    
                 let mailformat =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -18,4 +18,22 @@
                 
 
         }
-       
+  //validate password     
+  function validatepwd()
+  {
+          let password = document.getElementById('pwd');    
+          let passwordformat =  /^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*).{8,}$/;
+          if (!passwordformat.test(password.value)) 
+          {
+              alert('Please enter a valid password');
+             
+              return false; 
+          }
+          else{
+              
+          return true;
+          }
+          
+
+  }
+  //
